92dbd8199cfd1570679e21c3e96f6daf31f49884
vikashkumar897 <77895062+vikashkumar897@users.noreply.github.com>
2022-02-26 10:05:41 +0530

    location based page added

--- swiggy-clone/src/App.js
+++ swiggy-clone/src/App.js
@@ -1,48 +1,46 @@
 import "./App.css";
-import {Routes,Route} from 'react-router-dom';
-import Menu from './components/Pages/Menu/Menu';
+import { Routes, Route } from "react-router-dom";
+import Menu from "./components/Pages/Menu/Menu";
 import Search from "./components/Pages/Search/Search";
-import PaymentOffers from './components/Pages/Offers/PaymentOffers';
-import RestaurantOffers from './components/Pages/Offers/RestaurantOffers';
-import LandingPage from './components/landingPage/LandingPage';
+import PaymentOffers from "./components/Pages/Offers/PaymentOffers";
+import RestaurantOffers from "./components/Pages/Offers/RestaurantOffers";
+import LandingPage from "./components/landingPage/LandingPage";
 import Cart from "./components/cart/Cart";
 import ReadPolicy from "./components/cart/ReadPolicy";
 import { RouteContext } from "./components/routeFolder/RouteContext";
 import Navbar from "./components/Navbar/Navbar";
 import { useContext, useState } from "react";
 import Help from "./components/Help/Help";
+import { LocationBased } from "./components/locationBased/LocationBased";
 
 function App() {
-
   return (
     <div className="App">
- <Navbar />
-     
+      <Navbar />
+
       <Routes>
-        <Route path='/' element={<LandingPage/> }></Route>
-        <Route path="/cart" element={<Cart /> }></Route>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
         <Route path="/cart/policy" element={<ReadPolicy />}></Route>
         {/* <Route path="/menu" element={<Menu />}></Route> */}
         {/* <Route path="/Menu" element={<Menu />}></Route> */}
-        <Route path='/search' element={<Search/>}></Route>
-        <Route path='/offers/restaurant' element={<RestaurantOffers/>}></Route>
-        <Route path='/offers/payment' element={<PaymentOffers/>}></Route> 
+        <Route path="/search" element={<Search />}></Route>
+        <Route path="/offers/restaurant" element={<RestaurantOffers />}></Route>
+        <Route path="/offers/payment" element={<PaymentOffers />}></Route>
         <Route path="/help" element={<Help />}></Route>
+        <Route path="/:location" element={<LocationBased />} />
       </Routes>
-
-     
     </div>
   );
 }
 
 export default App;
 
-
-function CartComponent(){
-  return (<div>
+function CartComponent() {
+  return (
+    <div>
       <Navbar />
       <Cart />
-    </div>)
+    </div>
+  );
 }
-
-
---
+++ swiggy-clone/src/components/locationBased/LocationBased.jsx
@@ -0,0 +1,132 @@
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./LocationBased.css";
+
+export const LocationBased = () => {
+  const { location } = useParams();
+  return (
+    <div className="locationBasedMainDiv">
+      <div className="locationBasedMainDiv1">
+        <div className="locationBasedMainDiv11">
+          <h1>Great restaurants in {location}, delivering to you</h1>
+          <div className="locationBasedMainDiv111">
+            Set exact location to find the right restaurants near you.
+          </div>
+          <div className="locationBasedMainSearchDiv">
+            Enter street name, area etc...
+            <div className="locationBasedMainSearchDiv2">Find Food</div>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ backgroundColor: "#f7f7f7" }}>
+        <div className="locationBasedMainDiv2">
+          <span>
+            Home / <span>{location}</span>
+          </span>
+        </div>
+      </div>
+
+      <div className="locationBasedMainAbout">
+        <div className="locationBasedMainAboutHeading">About the food culture in {location}</div>
+        <div className="locationBasedMainAboutDesc">
+          Order food & beverages online from restaurants near & around you. We deliver food from
+          your neighborhood local joints, your favorite cafes, luxurious & elite restaurants in your
+          area. Exciting bit? We place no minimum order restrictions! Order in as little (or as
+          much) as you?d like. We?ll Swiggy it to you!
+        </div>
+      </div>
+
+      <div className="locationBasedMainPicks">
+        <div className="locationBasedMainPicksHeading">Our picks for you</div>
+        <div className="locationBasedMainPicksCards">
+          <div className="locationBasedMainPicksCardRow">
+            <Link to="#" className="PicksCard">
+              <img
+                className="PicksCardImg"
+                src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/Offers_yqoiuk"
+                alt="Offers near you"
+              />
+              <div className="PicksCardHeading">Offers near you</div>
+            </Link>
+            <Link to="/" className="PicksCard">
+              <img
+                className="PicksCardImg"
+                src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/TopRated_kyu7dk"
+                alt="top rated"
+              />
+              <div className="PicksCardHeading">top rated</div>
+            </Link>
+            <Link to="/" className="PicksCard">
+              <img
+                className="PicksCardImg"
+                src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/SuperFastDelivery_lmh0wh"
+                alt="super fast delivery"
+              />
+              <div className="PicksCardHeading">super fast delivery</div>
+            </Link>
+          </div>
+
+          <div className="locationBasedMainPicksCardRow" style={{ marginTop: "30px" }}>
+            <Link to="/" className="PicksCard">
+              <img
+                className="PicksCardImg"
+                src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/Biriyani_bohvze"
+                alt="biryani"
+              />
+              <div className="PicksCardHeading">biryani</div>
+            </Link>
+            <Link to="/" className="PicksCard">
+              <img
+                className="PicksCardImg"
+                src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NorthIndian_u554zm"
+                alt="North Indian"
+              />
+              <div className="PicksCardHeading">North Indian</div>
+            </Link>
+            <Link to="/" className="PicksCard">
+              <img
+                className="PicksCardImg"
+                src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/SouthIndian_a5ivgy"
+                alt="South Indian"
+              />
+              <div className="PicksCardHeading">South Indian</div>
+            </Link>
+          </div>
+        </div>
+
+        <div className="locationBasedMainLocation">
+          <div className="locationBasedMainLocationHeading">Where might we find you?</div>
+        </div>
+        <div className="locationBasedMainRestaurants">
+          <div className="locationBasedMainRestaurantsHeading">
+            Popular restaurants in and around {location}
+          </div>
+          <div className="restaurantsCards">
+            <div className="restaurantsCard">
+              <Link to="/" className="restaurantsCardLink">
+                <div className="restaurantsCardLinkDiv">
+                  <div className="restaurantsCardLinkDivImg">
+                    <img
+                      src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/gbbky4jdkrshfpvghbwu"
+                      alt="img"
+                    />
+                  </div>
+                  <div style={{ marginTop: "14px" }}>
+                    <div style={{ fontSize: "170%", fontWeight: "500", wordBreak: "break-word" }}>
+                      Subway
+                    </div>
+                    <div style={{ color: "#686b78", fontSize: "130%", marginTop: "4px" }}>
+                      Salads, Snacks, Desserts, Beverages
+                    </div>
+                  </div>
+                  <div className="cardDetailsDiv"></div>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
